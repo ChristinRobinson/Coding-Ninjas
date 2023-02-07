f7c9039b2ac95f5f8956759ed6a0082c87e71548
ChristinRobinson <christinrobinson463@gmail.com>
2023-02-07 09:10:55 +0530

    setup

---
+++ Career Camp/Front End | Full Stack Web Development/Part 2/15 Javascript: Functions and Arrays/Assessments/02 Day of the Week/final/Solution.js
@@ -0,0 +1,15 @@
+var weekDay = [
+	'Sunday',
+	'Monday',
+	'Tuesday',
+	'Wednesday',
+	'Thursday',
+	'Friday',
+	'Saturday',
+];
+
+function returnDay(x) {
+	return x < 1 || x > 7 ? null : weekDay[x];
+}
+
+console.log(returnDay(3));
