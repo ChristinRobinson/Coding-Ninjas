75963770a0edcbb6eeed4d4e45c52172243cff19
ChristinRobinson <christinrobinson463@gmail.com>
2023-02-08 11:52:55 +0530

    setup

---
+++ Career Camp/Front End | Full Stack Web Development/Part 2/16 Objects and Timing Events/Assessments/Assignment/01 Create Timer/setup/timerSolutionCode.js
@@ -0,0 +1,10 @@
+function timer() {
+    for(let i = 1; i<=5; i++){
+        setTimeout(function() {
+           //Write your code here...
+          
+        }, //write your code here...);
+    }
+    console.log("Hello ninjas!");
+}
+timer();
